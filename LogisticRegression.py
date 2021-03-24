662d37d1d0590ab6edca65f62a47de7248263f20
liu__yang <2471001205@qq.com>
2021-03-24 23:39:00 +0800

    Add LogisticRegression

---
+++ LogisticRegression.py
@@ -0,0 +1,91 @@
+import numpy as np
+
+class LogisticRegression:
+    def __init__(self, lr=0.01, tol=1e-4, max_iter=1e3, fit_intercept=True):
+        """
+        通过梯度下降方法来求解逻辑回归的参数
+
+        Parameters
+        ----------
+        lr : float
+            梯度下降方法的学习率
+        tol : float
+            模型误差小于多少停止迭代
+        max_iter : int
+            最大迭代次数
+        fit_intercept : bool
+            是否在线性回归中，添加截距项。 默认为True
+        """
+        self.beta = None
+        self.lr = lr
+        self.tol = tol
+        self.max_iter = max_iter
+        self.fit_intercept = fit_intercept
+
+    def fit(self, X, y):
+        """
+        使用梯度下降法求解模型参数
+        """
+        if self.fit_intercept:
+            X = np.c_[np.ones(X.shape[0]), X]
+
+        prev_loss = np.inf
+        self.beta = np.random.rand(X.shape[1])
+        for _ in range(int(self.max_iter)):
+            y_prob = self.sigmoid(X @ self.beta)
+            loss = self.loss(X, y, y_prob)
+            print("loss:",loss)
+            if prev_loss - loss < self.tol:
+                break
+            prev_loss = loss
+            self.beta -= self.lr * self.grad(X, y, y_prob)
+
+    def predict(self, X):
+        """
+        使用模型进行预测
+        """
+        if self.fit_intercept:
+            X = np.c_[np.ones(X.shape[0]), X]
+        y_prob = self.sigmoid(X @ self.beta)
+        return y_prob
+
+    def grad(self, X, y, y_prob):
+        """
+        使用梯度下降法更新参数
+        """
+        N, M = X.shape
+        return np.dot(X.T, (y_prob - y)) / N
+
+    
+    def loss(self, X, y, y_prob):
+        """
+        模型的交叉熵误差
+        """
+        N, M = X.shape
+        loss = - y * np.log(y_prob) - (1 - y) * np.log(1 - y_prob)
+        return np.sum(loss) / N
+
+    def sigmoid(self, x):
+        """
+        逻辑函数
+        """
+        return 1 / (1 + np.exp(-x))
+
+if __name__ == "__main__":
+    import pandas as pd
+    data = pd.read_csv("data/iris0.csv")
+    data = data.sample(frac=1).reset_index(drop=True)
+
+    labels = data.pop("class")
+    y = labels.values - 1
+    clf = LogisticRegression(lr=0.01)
+    clf.fit(data.values,y)
+    preds = clf.predict((data.values))
+    y_pred = np.where(preds > 0.5,1,0)
+    print( sum(y == y_pred) / len(y) )
+
+    from sklearn.linear_model import LogisticRegression as LR
+    lr = LR()
+    lr.fit(data.values,y)
+    y_pred = lr.predict(data.values)
+    print( sum(y == y_pred) / len(y) )
